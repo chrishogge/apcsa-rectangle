2265d11f34c736db012840fe839ad1b6572761c9
Chris Hogge <chogge@ucvts.org>
2019-10-02 14:10:11 -0400

    Completed

--- src/apcsa_rectangle.java
+++ src/apcsa_rectangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class apcsarectangle {
+public class apcsa_rectangle {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
